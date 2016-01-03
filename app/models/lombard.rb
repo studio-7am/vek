442bee6058e963116c98a42405c21d911e7cdb37
bjsmth <bj7smth@gmail.com>
2016-01-03 16:30:23 +1000

    add uploaders

--- app/models/lombard.rb
+++ app/models/lombard.rb
@@ -1,4 +1,5 @@
 class Lombard < ActiveRecord::Base
 	has_many :lombard_tabs
 	has_many :lombard_orders
+	mount_uploader :image, LombardUploader
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,4 @@
 class Store < ActiveRecord::Base
 	has_many :store_tabs
+	mount_uploader :image, StoreUploader
 end
